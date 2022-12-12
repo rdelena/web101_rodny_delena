9514819e233150e0acbbddd4074651b1c5b15244
rdelena <rodnydelena@gmail.com>
2022-12-11 23:22:27 -0600

    added parallax effect on landing page & JS type animation

--- scripts.js
+++ scripts.js
@@ -4,3 +4,10 @@ new URLSearchParams(window.location.search).forEach((value, name) => {
   placeForResults.append(`${name} : ${value}`);
   placeForResults.append(document.createElement("br"));
 });
+
+let typed = new Typed(".auto-type", {
+  strings: ["Software Engineer", "Web Developer", "Medical Professional"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
